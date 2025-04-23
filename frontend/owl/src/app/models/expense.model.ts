16eb431b7b16538912c7e604e74a4ea88b54bdfa
XXX1694 <serikbay.a04@gmail.com>
2025-04-23 22:27:37 +0500

    project update2

--- frontend/owl/src/app/models/expense.model.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from './category.model';
-
-export interface Expense {
-  id?: number;
-  amount: number;
-  category: string | Category;
-  date: string;
-  description?: string;
-}
